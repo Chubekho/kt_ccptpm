5fa03b7ce43bb6c02e58f290cec46913e505560c
tronghieu2101 <giaptronghieu165@gmail.com>
2025-12-11 11:01:42 +0700

    fix(api): hieu fix api thanh 8084

--- client/src/pages/ProductManager/index.jsx
+++ client/src/pages/ProductManager/index.jsx
@@ -3,7 +3,7 @@ import ProductList from './components/ProductList';
 import ProductForm from './components/ProductForm';
 import styles from './ProductManager.module.scss'; // Import SCSS module
 
-const API_URL = 'http://localhost:8082/api/product'; // Đảm bảo bạn đã config proxy trong vite.config.js hoặc dùng http://localhost:PORT/api/product
+const API_URL = 'http://localhost:8084/api/product'; // Đảm bảo bạn đã config proxy trong vite.config.js hoặc dùng http://localhost:PORT/api/product
 
 const ProductManager = () => {
   const [products, setProducts] = useState([]);
